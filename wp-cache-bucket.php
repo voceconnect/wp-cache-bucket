7e2f5505a7daba2759cbfbbc4e5569262230e24c
Curtis Loisel <curtis@voceconnect.com>
2013-07-30 16:40:22 -0700

    Wrapping with class check

--- wp-cache-bucket.php
+++ wp-cache-bucket.php
@@ -8,69 +8,73 @@
  * @author prettyboymp
  */
 
-class WP_Cache_Bucket {
+if ( !class_exists('WP_Cache_Bucket') ) {
 
-	public static function bucket_add($bucket, $key, $data, $group = '', $expire = 0) {
-		$structured_data = array(
-			'key' => self::_get_cache_bucket_key($bucket, $group), 
-			'data' => $data
-		);
-		
-		return wp_cache_add($key, $structured_data, $group, $expire);		
-	}
+	class WP_Cache_Bucket {
 
-	public static function bucket_set($bucket, $key, $data, $group = '', $expire = 0) {
-		$structured_data = array(
-			'key' => self::_get_cache_bucket_key($bucket, $group), 
-			'data' => $data
-		);
-		
-		return wp_cache_set($key, $structured_data, $group, $expire);		
-	}
-
-	public static function bucket_get($bucket, $key, $group = '', $force = false) {
-		$structured_data = wp_cache_get($key, $group, $force);
-		if(is_array($structured_data) && isset($structured_data['key']) &&
-			($structured_data['key'] === self::_get_cache_bucket_key( $bucket , $group))) {
-			return $structured_data['data'];
+		public static function bucket_add($bucket, $key, $data, $group = '', $expire = 0) {
+			$structured_data = array(
+				'key' => self::_get_cache_bucket_key($bucket, $group), 
+				'data' => $data
+			);
+			
+			return wp_cache_add($key, $structured_data, $group, $expire);		
 		}
-		return false;		
-	}
 
-	public static function bucket_flush($bucket, $group = '') {
-		wp_cache_delete('wpcb_key_'. $bucket, $group);
-	}	
-
-	/**
-	 * Returns the key that validates the bucket
-	 * @param string $bucket
-	 * @param string $group
-	 * @return string
-	 */
-	private function _get_cache_bucket_key($bucket, $group = '') {
-		$key = wp_cache_get('wpcb_key_'. $bucket, $group);
-		if(false === $key) {
-			$key = md5( uniqid( microtime() . mt_rand(), true ) );
-			wp_cache_set('wpcb_key_'. $bucket, $key, $group);
+		public static function bucket_set($bucket, $key, $data, $group = '', $expire = 0) {
+			$structured_data = array(
+				'key' => self::_get_cache_bucket_key($bucket, $group), 
+				'data' => $data
+			);
+			
+			return wp_cache_set($key, $structured_data, $group, $expire);		
 		}
-		
-		return $key;
+
+		public static function bucket_get($bucket, $key, $group = '', $force = false) {
+			$structured_data = wp_cache_get($key, $group, $force);
+			if(is_array($structured_data) && isset($structured_data['key']) &&
+				($structured_data['key'] === self::_get_cache_bucket_key( $bucket , $group))) {
+				return $structured_data['data'];
+			}
+			return false;		
+		}
+
+		public static function bucket_flush($bucket, $group = '') {
+			wp_cache_delete('wpcb_key_'. $bucket, $group);
+		}	
+
+		/**
+		 * Returns the key that validates the bucket
+		 * @param string $bucket
+		 * @param string $group
+		 * @return string
+		 */
+		private function _get_cache_bucket_key($bucket, $group = '') {
+			$key = wp_cache_get('wpcb_key_'. $bucket, $group);
+			if(false === $key) {
+				$key = md5( uniqid( microtime() . mt_rand(), true ) );
+				wp_cache_set('wpcb_key_'. $bucket, $key, $group);
+			}
+			
+			return $key;
+		}
+
 	}
 
-}
+	function wp_cache_bucket_add($bucket, $key, $data, $group = '', $expire = 0) {
+		return WP_Cache_Bucket::bucket_add($bucket, $key, $data, $group, $expire);
+	}
 
-function wp_cache_bucket_add($bucket, $key, $data, $group = '', $expire = 0) {
-	return WP_Cache_Bucket::bucket_add($bucket, $key, $data, $group, $expire);
-}
+	function wp_cache_bucket_set($bucket, $key, $data, $group = '', $expire = 0) {
+		return WP_Cache_Bucket::bucket_set($bucket, $key, $data, $group, $expire);
+	}
 
-function wp_cache_bucket_set($bucket, $key, $data, $group = '', $expire = 0) {
-	return WP_Cache_Bucket::bucket_set($bucket, $key, $data, $group, $expire);
-}
+	function wp_cache_bucket_get($bucket, $key, $group = '', $force = false) {
+		return WP_Cache_Bucket::bucket_get($bucket, $key, $group, $force);
+	}
 
-function wp_cache_bucket_get($bucket, $key, $group = '', $force = false) {
-	return WP_Cache_Bucket::bucket_get($bucket, $key, $group, $force);
-}
+	function wp_cache_bucket_flush($bucket, $group = '') {
+		WP_Cache_Bucket::bucket_flush($bucket, $group);
+	}
 
-function wp_cache_bucket_flush($bucket, $group = '') {
-	WP_Cache_Bucket::bucket_flush($bucket, $group);
 }
